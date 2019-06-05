8015946435349bad8403440617cf73aa47143628
woodywoodpecker <1210513673@qq.com>
2019-06-05 23:28:45 +0800

    initial py4j-admin-frontend

---
+++ py4j-admin-frontend/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is login component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
--- py4j-admin-frontend/src/main.js
+++ py4j-admin-frontend/src/main.js
@@ -4,12 +4,34 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入nprogress进度条
+// import NProgress from 'nprogress'
+// 引入nprogress进度条的样式
+import 'nprogress/nprogress.css'
+
+// 导入axios
+import axios from 'axios'
+
+// 引入字体图标
+import './assets/iconfont/iconfont.css'
+// 公共样式
+import './assets/css/public.css'
+
 // element ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入登录组件
+const Login = () => import(/* webpackChunkName: "home" */ '@/components/login')
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+// 把axios对象绑定到Vue原型中全局使用
+Vue.prototype.axios = axios
+
+Vue.use(router)
 
 /* eslint-disable no-new */
 new Vue({
